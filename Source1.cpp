40f7a81c84750064859d8276bd1b5e36c54d7481
VIET <VIET@DESKTOP-JH8NPO8>
2020-12-28 21:19:59 +0700

    vs

---
+++ Source1.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+using namespace std;
+
+void main()
+{
+    int a, b = 0;
+    int tong = 0;
+    int hieu = 0;
+    cout << "nhap a";
+    cin >> a;
+    cout << "nhap b";
+    cin >> b;
+    tong = a + b;
+    hieu = a - b;
+    cout << "tong la" << tong;
+    cout << "hieu la" << tong;
+    cout << "tong la" << tong;
+    cout << "hieu la" << tong;
+}asdsadsadadasd
