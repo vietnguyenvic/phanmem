f18bba90f8e0b15d85ef90ee2d7e024541935c11
VIET <VIET@DESKTOP-JH8NPO8>
2020-12-28 21:23:41 +0700

    xuat ra hieu

--- Source1.cpp
+++ Source1.cpp
@@ -4,16 +4,16 @@ using namespace std;
 void main()
 {
     int a, b = 0;
-    int tong = 0;
+    int hieu = 0;
     int hieu = 0;
     cout << "nhap a";
     cin >> a;
     cout << "nhap b";
     cin >> b;
-    tong = a + b;
+    hieu = a + b;
     hieu = a - b;
-    cout << "tong la" << tong;
-    cout << "hieu la" << tong;
-    cout << "tong la" << tong;
-    cout << "hieu la" << tong;
-}asdsadsadadasd
+    cout << "hieu la" << hieu;
+    cout << "hieu la" << hieu;
+    cout << "hieu la" << hieu;
+    cout << "hieu la" << hieu;
+}
