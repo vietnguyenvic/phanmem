f1846ab84452b9b2481a1757533dd886d63c5216
VIET <VIET@DESKTOP-JH8NPO8>
2020-12-28 21:33:05 +0700

    hihi

---
+++ Source1.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+using namespace std;
+
+void main()
+{
+    int a, b = 0;
+    int hieu = 0;
+    int hieu = 0;
+    cout << "nhap a";
+    cin >> a;
+    cout << "nhap b";
+    cin >> b;
+    hieu = a + b;
+    hieu = a - b;
+    cout << "hieu la" << hieu;
+    cout << "hieu la" << hieu;
+    cout << "hieu la" << hieu;
+    cout << "hieu la" << hieu;
+}
